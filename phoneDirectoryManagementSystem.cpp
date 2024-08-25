549bb09b6b5fa3478d2d0507f5cafa6866c09d9e
Floro Lorenzo Gagni <enzopro2222@gmail.com>
2024-08-25 16:37:44 +0800

    Update phoneDirectoryManagementSystem.cpp

--- phoneDirectoryManagementSystem.cpp
+++ phoneDirectoryManagementSystem.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 using namespace std;
+namespace fs = std::filesystem;
+
 
 int lastIndex = 0;
 
@@ -9,130 +11,154 @@ class Directory
 public:
     long phoneno;
     string fname, lname;
-    Directory(int phoneno = 0, string fname = "", string lname = "")
-    {
-        this->phoneno = phoneno;
-        this->fname = fname;
-        this->lname = lname;
-    }
+
+    Directory(long phoneno = 0, string fname = "", string lname = "")
+        : phoneno(phoneno), fname(fname), lname(lname) {}
 };
 
-void addDetail(Directory &d, int flag)
-{
-    string specific = "";
-    if (flag)
-        specific = " New";
 
-    cout << "Enter" << specific << " First Name: ";
-    cin >> d.fname;
-    cout << "\nEnter" << specific << " Last Name: ";
-    cin >> d.lname;
-    cout << "\nEnter" << specific << " Phone No: ";
-    cin >> d.phoneno;
-}
-
-void displayDetails(Directory d[])
-{
-    cout << "\nFirstName\tLastName\tPhoneno\t\n";
-    for (int i = 0; i < lastIndex; i++)
-    {
-        if (d[i].fname != "Deleted")
-        {
-            cout
-                << "\n"
-                << d[i].fname << "\t\t" << d[i].lname << "\t\t" << d[i].phoneno << "\t\n";
-        }
+void listAllFiles(const string& path) { // TO see the list files
+    cout << "List of all Files:\n";
+    for (const auto& entry : fs::directory_iterator(path)) {
+        cout << entry.path().filename().string() << endl;
     }
 }
 
-void modifyDetail(Directory d[])
-{
-    string fname, lname;
-    cout << "To Modify Details: \n\n";
-    cout << "Enter First Name: ";
-    cin >> fname;
-    cout << "\nEnter Last Name: ";
-    cin >> lname;
-    cout << endl;
-    int flag = 0;
-    for (int i = 0; i < lastIndex; i++)
-    {
-        if (d[i].fname == fname && d[i].lname == lname)
-        {
-            flag = 1;
-            addDetail(d[i], 1);
+void listFilesByExtension(const string& path, const string& extension) { // to see list of  extension files
+    cout << "Listing Files with Extension: " << extension << endl;
+    int count = 0;
+    for (const auto& entry : fs::directory_iterator(path)) {
+        if (fs::is_regular_file(entry) && entry.path().extension() == extension) {
+            cout << entry.path().filename().string() << endl;
+            count++;
         }
     }
-    if (flag)
-        cout << "\nDetails Modified Successfully..\n\n";
-    else
-        cout << "\nNo Details Found..\n\n";
+    if (count == 0)
+        cout << "No files found with extension " << extension << endl;
 }
 
-void deleteDetail(Directory d[])
-{
-    string fname, lname;
-    cout << "To Delete Details: \n\n";
-    cout << "Enter First Name: ";
-    cin >> fname;
-    cout << "\nEnter Last Name: ";
-    cin >> lname;
-    cout << endl;
-    int flag = 0;
-    for (int i = 0; i < lastIndex; i++)
-    {
-        if (d[i].fname == fname && d[i].lname == lname)
-        {
-            flag = 1;
-            d[i].fname = "Deleted";
-            d[i].lname = "Deleted";
-            d[i].phoneno = 0;
+void listFilesByPattern(const string& path, const string& pattern) { // too see files from extension
+    cout << "Listing Files Matching Pattern: " << pattern << endl;
+    int count = 0;
+    for (const auto& entry : fs::directory_iterator(path)) {
+        if (fs::is_regular_file(entry) && entry.path().filename().string().find(pattern) != string::npos) {
+            cout << entry.path().filename().string() << endl;
+            count++;
         }
     }
-    if (flag)
-        cout << "\nDetails Delete Successfully..\n\n";
-    else
-        cout << "\nNo Details Found..\n\n";
+    if (count == 0)
+        cout << "No files found matching pattern " << pattern << endl;
 }
 
-int main()
-{
-    const int count = 100;
-    Directory d[count];
-    int ch;
-    cout << "\nWelcome to Phone Directory Management System\n";
-    do
-    {
-        cout << "\nSelect an Operation: \n\n1.Add New Details\n2.Display All Details\n3.Modify Specific Detail\n4.Delete a Details\n5.Exit\n\nEnter your Choice: ";
-        cin >> ch;
-        cout << endl;
-        switch (ch)
-        {
+void createDirectory(const string& path) { // To create new directory
+    if (fs::create_directory(path)) {
+        cout << "Directory Successfully Created:" << path << endl;
+    } else {
+        cout << "Error" << endl;
+    }
+}
+
+void changeDirectory(string& path) { // To change directory
+    int choice;
+    cout << "Change Directory Options:\n";
+    cout << "1. Step by step backward. \n";
+    cout << "2.Goto Root Directory.\n";
+    cout << "3. Forward directory. \n";
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    switch (choice) {
         case 1:
-            addDetail(d[lastIndex], 0);
-            lastIndex++;
-            cout << "\nDetails Added Successfully..\n\n";
+            path = fs::current_path().parent_path().string();
             break;
         case 2:
-            displayDetails(d);
+            path = fs::path("/").string();
             break;
-        case 3:
-            modifyDetail(d);
-            break;
-        case 4:
-            deleteDetail(d);
-            break;
-        case 5:
-            cout << "Thank You.." << endl
-                 << endl;
-            return 0;
-            break;
-        default:
-            cout << "\nYou entered an ßInvalid Option...\n";
+        case 3: {
+            string newPath;
+            cout << "Enter the path of the directory: ";
+            cin >> newPath;
+            if (fs::exists(newPath) && fs::is_directory(newPath)) {
+                path = newPath;
+            } else {
+                cout << "Invalid path or directory does not exist." << endl;
+            }
             break;
         }
-    } while (ch != 5 && lastIndex < count - 1);
+        default:
+            cout << "Invalid option!" << endl;
+    }
+    fs::current_path(path); // Update the current working directory
+    cout << "Current directory changed to: " << fs::current_path() << endl;
+}
 
-    cout << endl;
+int main() {
+
+    string currentPath = fs::current_path().string();
+    int ch;
+
+    cout << "\nWelcome to Phone Directory and File Management System\n";
+    do {
+        cout << "\nMain Menu\n";
+        cout << "_____________\n";
+        cout << "1. Display List of Files\n";
+        cout << "2. Create a New Directory\n";
+        cout << "3. Change the Working Directory\n";
+        cout << "4. Exit\n";
+        cout << "Enter The Number: ";
+        cin >> ch;
+
+        switch (ch) { // main menu choices
+            case 1: {
+                int subChoice;
+                cout << "\nList Files Detail\n";
+                cout << "\n_______________\n";
+                cout << "1. List all files .\n";
+                cout << "2. List of extension files.\n";
+                cout << "3. List of Name Wise     \n";
+                cout << "Enter your choice: ";
+                cin >> subChoice;
+
+                switch (subChoice) { // DIsplay List of files choices
+                    case 1:
+                        listAllFiles(currentPath);
+                        break;
+                    case 2: {
+                        string extension;
+                        cout << "Enter the file extension (e.g., .txt): ";
+                        cin >> extension;
+                        listFilesByExtension(currentPath, extension);
+                        break;
+                    }
+                    case 3: {
+                        string pattern;
+                        cout << "Enter the file name pattern (e.g., moha*.*): ";
+                        cin >> pattern;
+                        listFilesByPattern(currentPath, pattern);
+                        break;
+                    }
+                    default:
+                        cout << "Invalid option!" << endl;
+                }
+                break;
+            }
+            case 2: { // main menu choices
+                string dirName;
+                cout << "Enter the name of the directory name: ";
+                cin >> dirName;
+                createDirectory(currentPath + "/" + dirName);
+                break;
+            }
+            case 3: // main menu choices
+                changeDirectory (currentPath);
+                break;
+            case 4: // main menu choices
+                cout << "Thank you for using the system. Exiting...\n";
+                return 0;
+            default:
+                cout << "Invalid option! Please try again.\n";
+        }
+    } while (ch != 4);
+
     return 0;
 }
